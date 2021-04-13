6e937f061739c70ab28d25a361377e6660e5e014
xinggevip <1511844263@qq.com>
2021-04-13 17:17:18 +0800

    进入活动

--- src/main/java/com/xinggevip/controller/yewuController.java
+++ src/main/java/com/xinggevip/controller/yewuController.java
@@ -1,7 +1,11 @@
 package com.xinggevip.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
 import com.xinggevip.dao.PlayerMapper;
 import com.xinggevip.domain.Invitation;
+import com.xinggevip.domain.Judge;
+import com.xinggevip.domain.Player;
 import com.xinggevip.domain.User;
 import com.xinggevip.enunm.ResultCodeEnum;
 import com.xinggevip.exception.EmpLoginException;
@@ -18,11 +22,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
 
 @Api(tags = {""})
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/yewu")
 @CrossOrigin
 public class yewuController {
 
@@ -46,7 +51,7 @@ public class yewuController {
             @ApiImplicitParam(name = "invitationCode", value = "验证码"),
             @ApiImplicitParam(name = "name", value = "姓名")
     })
-    @PostMapping("/login")
+    @PostMapping("/join")
     public HttpResult login(@RequestParam String invitationCode,@RequestParam String name) {
         // 获取验证码类型
         List<Invitation> invitationList = invitationService.lambdaQuery().eq(Invitation::getInvitationCode, invitationCode).list();
@@ -54,15 +59,52 @@ public class yewuController {
             return HttpResult.failure(ResultCodeEnum.NOFIND_CODE_ERRORTWO);
         }
 
-        String codeType = invitationList.get(0).getInvitationType();
+        Invitation invitation = invitationList.get(0);
+        String codeType = invitation.getInvitationType();
+        Integer activateId = invitation.getActivateId();
+        // 验证码为选手
         if ("1".equals(codeType)) {
             // 插入或更新选手
-            Invitation invitation = new Invitation();
+            QueryWrapper<Player> wrapper = new QueryWrapper<>();
+            wrapper.eq("name", name);
+            Player player1 = new Player();
+            Player player2 = player1.selectOne(wrapper);
+            if (player2 == null) {
+                Player player = new Player();
+                player.setName(name);
+                player.setActivateId(activateId);
+                boolean b = player.insertOrUpdate();
+                System.out.println("验证码为选手，insertOrUpdate的结果"+String.valueOf(b));
+            }
+        }
+        // 验证码为评委
+        if ("2".equals(codeType)) {
+
+            QueryWrapper<Judge> wrapper = new QueryWrapper<>();
+            wrapper.eq("judge_name", name);
+            Judge judge1 = new Judge();
+            Judge judge2 = judge1.selectOne(wrapper);
+            if (judge2 == null) {
+                Judge judge = new Judge();
+                judge.setJudgeName(name);
+                judge.setActivateId(activateId);
+                boolean b = judge.insertOrUpdate();
+                System.out.println("验证码为评委，insertOrUpdate的结果"+String.valueOf(b));
+            }
 
 
         }
+        // 验证码为场控
+        if ("3".equals(codeType)) {
+            System.out.println("验证码为场控");
+        }
 
-        return null;
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("name",name);
+        map.put("codeType", codeType);
+        HttpResult<Object> httpResult = HttpResult.success(map);
+
+        return httpResult;
 
     }
 }
